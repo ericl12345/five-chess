ec7de81db08faeec92c45517b0d28271d97d8093
ericl12345 <ericliu100322@gmail.com>
2021-10-19 18:40:09 +1300

    Check vertical
    Check win on vertical.

--- checkVertical.py
+++ checkVertical.py
@@ -3,19 +3,26 @@ from checkVerticalLine import check_vertical_line
 
 def check_vertical(player_selection, size, board):
     board_line = []
-    for g in range(size):
-        board_line.append(board[g * size])
 
     for i in range(size):
+        for g in range(size):
+            board_line.append(board[g * size + i])
+        
         if (check_vertical_line(player_selection, board_line)):
             return True
     return False
 
 if __name__ == "__main__":
     size = 9
-    board = ['B' for x in range(size * size)]
-    board[5:11] = [" " for x in range(6)]
-    board[45:55] = [" " for x in range(10)]
+    board = [" " for x in range(size * size)]
+
+    a = 7
+    board[0+a] = "B"
+    board[size+a] = "B"
+    board[2*size+a] = "B"
+    board[3*size+a] = "B"
+    board[4*size+a] = "B"
+
     print(board)
     player_selection = "B"
 
