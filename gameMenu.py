4774e0dc51b8c2ce542c9458b143cd6a7ec12feb
ericl12345 <ericliu100322@gmail.com>
2021-10-21 14:39:30 +1300

    Game menu
    Game menu.

---
+++ gameMenu.py
@@ -0,0 +1,34 @@
+import askBoardLength
+from askBoardLength import ask_board_length
+import getPlayerNames
+from getPlayerNames import get_player_names
+
+def gameMenu(player):
+    while True:
+        # Player choice Menu
+        print("Turn to choose for {}" .format(player))
+        print("Enter 1 for Black")
+        print("Enter 2 for White")
+        print("Enter 3 to Quit")
+ 
+        # Try exception for CHOICE input
+        try:
+            global choice
+            choice = int(input())   
+        except ValueError:
+            print("Wrong Input!!! Try Again\n")
+            continue
+        if (choice < 1 or choice > 3):
+            print("Wrong Input!!! Try Again\n")
+            continue
+        elif choice == 1 or choice == 2:
+            lengthOfBoard = ask_board_length()
+        else:
+            lengthOfBoard = "q"
+        return [choice, lengthOfBoard]
+
+if __name__ == "__main__":
+    player = get_player_names
+    result = gameMenu(player)
+
+    print(result)
